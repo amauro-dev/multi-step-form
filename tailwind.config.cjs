66156ed050cc80acac24854b8eaebfa42f0c652f
Andrés Mauro <amauro@192.168.1.4>
2026-02-27 14:37:55 -0300

    setting up tailwind css

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./apps/**/*.{html,js,ts,jsx,tsx}",
+    "./packages/ui/src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
