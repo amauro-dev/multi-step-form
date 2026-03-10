1ca7690b5ec112a8c71dba6c78dfe59b43cabac4
Andrés Mauro <amauro@192.168.1.4>
2026-03-09 22:52:56 -0300

    monorepo architecture improvements

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,6 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import { PageLayout } from "@multi-step-form/ui";
-function App() {
-  // const [count, setCount] = useState(0);
 
+function App() {
   return (
     <PageLayout>
       <h1 className="text-3xl font-bold underline bg-red-200">Hello world!</h1>
@@ -13,3 +9,4 @@ function App() {
 }
 
 export default App;
+
