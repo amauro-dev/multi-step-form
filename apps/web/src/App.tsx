7fa29ab49aa654ee2d4d3666452a3bb476d0ce31
Andrés Mauro <amauro@192.168.1.4>
2026-03-09 23:13:48 -0300

    wip: layout

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,7 +3,9 @@ import { PageLayout } from "@multi-step-form/ui";
 function App() {
   return (
     <PageLayout>
-      <h1 className="text-3xl font-bold underline bg-red-200">Hello world!</h1>
+      <h1 className="text-3xl font-bold text-blue-900">Title</h1>
+      <p className="text-gray-500">Some description</p>
+      <button>Next Step</button>
     </PageLayout>
   );
 }
--- packages/ui/src/components/pageLayout.tsx
+++ packages/ui/src/components/pageLayout.tsx
@@ -1,5 +1,12 @@
 export const PageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-green-500 text-red-500 h-screen w-screen">{children}</div>
+    <>
+    <div className="bg-white h-screen w-screen flex p-4 gap-4">
+      <div className="bg-violet-700 w-1/3 rounded-xl"></div>
+      <div className="bg-gray-200 w-screen p-12 mx-20">
+      { children }
+      </div>
+    </div>
+    </>
   );
 };
