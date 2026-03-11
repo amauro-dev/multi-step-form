6d9c2cc6f5bde4c0123770d9ae469c78e5faf66b
Andrés Mauro <amauro@192.168.1.4>
2026-03-10 23:14:21 -0300

    page layout for mobile

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -5,7 +5,6 @@ function App() {
     <PageLayout>
       <h1 className="text-3xl font-bold text-blue-900">Title</h1>
       <p className="text-gray-500">Some description</p>
-      <button>Next Step</button>
     </PageLayout>
   );
 }
--- packages/ui/src/components/pageLayout.tsx
+++ packages/ui/src/components/pageLayout.tsx
@@ -1,18 +1,23 @@
 
-
-
 export const PageLayout = ({ children }: { children: React.ReactNode }) => {
-
+    // MOBILE
   return (
-    <>
-    <div className="bg-white h-screen w-screen flex flex-col p-4 gap-4">
-      {/* <div className="bg-violet-700 w-1/3 rounded-xl"></div> */}
-      <div className="bg-[#EBF2FC] my-20 rounded-xl flex-1">
-      { children }
+    <div className=" flex flex-col h-screen w-screen bg-red-200">
+      <div className="relative flex flex-col h-full">
+        <div className="bg-[url('../../assets/images/bg-sidebar-mobile.svg')] bg-cover bg-center top-0 h-1/4">
+        </div>
+        <div className="bg-[#EBF2FC] flex-1"></div>
+        {/* form */}
+        <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+          <div className="flex flex-1 flex-col bg-white mx-4 p-4 rounded-xl h-2/3">
+          { children }
+          </div> 
+        </div>
       </div>
-      <div className="bg-red-200 bottom-0">NAv</div>
+      <div className="flex flex-row bg-white bottom-0 p-4 justify-end">
+        <button className="bg-[#03295A]">Next Step</button>
+      </div>
     </div>
-    </>
   )
   
   // WEB
