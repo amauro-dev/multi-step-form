1b4c6a89d6c03a47cfe8ef7f9d95c60c88a34a0b
Andrés Mauro <amauro@192.168.1.4>
2026-03-10 23:13:42 -0300

    page layout for mobile

--- packages/ui/src/components/pageLayout.tsx
+++ packages/ui/src/components/pageLayout.tsx
@@ -1,4 +1,21 @@
+
+
+
 export const PageLayout = ({ children }: { children: React.ReactNode }) => {
+
+  return (
+    <>
+    <div className="bg-white h-screen w-screen flex flex-col p-4 gap-4">
+      {/* <div className="bg-violet-700 w-1/3 rounded-xl"></div> */}
+      <div className="bg-[#EBF2FC] my-20 rounded-xl flex-1">
+      { children }
+      </div>
+      <div className="bg-red-200 bottom-0">NAv</div>
+    </div>
+    </>
+  )
+  
+  // WEB
   return (
     <>
     <div className="bg-white h-screen w-screen flex p-4 gap-4">
